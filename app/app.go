4f57d93efb03513715a5d8c785998cd6b4d7f2a7
Potiguar Faga <potz@potz.me>
2015-10-07 20:04:50 +0000

    Render HTML content specified in posted JSON

--- app/app.go
+++ app/app.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
         "fmt"
         "os/exec"
+        "encoding/json"
+        "io"
 )
 
 func main() {
@@ -11,6 +13,10 @@ func main() {
         http.HandleFunc("/", requestHandler)
         fmt.Println("Http server listening on", bindAddress)
         http.ListenAndServe(bindAddress, nil)
+}
+
+type documentRequest struct {
+        Content string
 }
 
 func requestHandler(response http.ResponseWriter, request *http.Request) {
@@ -25,9 +31,26 @@ func requestHandler(response http.ResponseWriter, request *http.Request) {
                 fmt.Println(request.Method, request.URL, "405 not allowed")
                 return
         }
-        defer fmt.Println(request.Method, request.URL, "200 OK")
-        cmd := exec.Command("/usr/local/bin/wkhtmltopdf", "http://www.google.com/", "-")
+        decoder := json.NewDecoder(request.Body)
+        var req documentRequest
+        if err := decoder.Decode(&req); err != nil {
+                response.WriteHeader(http.StatusBadRequest)
+                fmt.Println(request.Method, request.URL, "400 bad request (invalid JSON)")
+                return
+        }
+        cmd := exec.Command("/usr/local/bin/wkhtmltopdf", "-", "-")
         response.Header().Set("Content-Type", "application/pdf")
         cmd.Stdout = response
-        cmd.Run()
+        stdin, err := cmd.StdinPipe()
+        if err != nil {
+                response.WriteHeader(http.StatusInternalServerError)
+                fmt.Println(request.Method, request.URL, "500 internal server error")
+                fmt.Println("Error dump =", err)
+                return
+        }
+        cmd.Start()
+        defer cmd.Wait()
+        io.WriteString(stdin, req.Content)
+        stdin.Close()
+        fmt.Println(request.Method, request.URL, "200 OK")
 }
