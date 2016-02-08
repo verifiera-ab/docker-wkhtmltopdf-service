ecf7cab94bbe43b4b8280fceeb220f340918c263
Potiguar Faga <potz@potz.me>
2016-02-08 15:23:42 -0200

    Improve logging, output remote address

--- app/app.go
+++ app/app.go
@@ -20,30 +20,35 @@ type documentRequest struct {
 	Options map[string]interface{}
 }
 
+func logOutput(request *http.Request, message string) {
+	ip := strings.Split(request.RemoteAddr, ":")[0]
+	fmt.Println(ip, request.Method, request.URL, message)
+}
+
 func requestHandler(response http.ResponseWriter, request *http.Request) {
 	if request.URL.Path != "/" {
 		response.WriteHeader(http.StatusNotFound)
-		fmt.Println(request.Method, request.URL, "404 not found")
+		logOutput(request, "404 not found")
 		return
 	}
 	if request.Method != "POST" {
 		response.Header().Set("Allow", "POST")
 		response.WriteHeader(http.StatusMethodNotAllowed)
-		fmt.Println(request.Method, request.URL, "405 not allowed")
+		logOutput(request, "405 not allowed")
 		return
 	}
 	decoder := json.NewDecoder(request.Body)
 	var req documentRequest
 	if err := decoder.Decode(&req); err != nil {
 		response.WriteHeader(http.StatusBadRequest)
-		fmt.Println(request.Method, request.URL, "400 bad request (invalid JSON)")
+		logOutput(request, "400 bad request (invalid JSON)")
 		return
 	}
 	segments := make([]string, 0)
 	for key, element := range req.Options {
 		if element == true {
 			// if it was parsed from the JSON as an actual boolean, 
-			// convert to command-line single argument  (--foo)
+			// convert to command-line single argument	(--foo)
 			segments = append(segments, fmt.Sprintf("--%v", key))
 		} else if element != false {
 			// Otherwise, use command-line argument with value (--foo bar)
@@ -58,5 +63,5 @@ func requestHandler(response http.ResponseWriter, request *http.Request) {
 	cmd.Stdout = response
 	cmd.Start()
 	defer cmd.Wait()
-	fmt.Println(request.Method, request.URL, "200 OK")
+	logOutput(request, "200 OK")
 }
